3d0a1bf63bb54a91d046a6da46c8ba6426fa33f1
l1payy <muhammadhidayat0278@gmail.com>
2026-03-24 23:43:26 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,4 +21,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    public function boot(): void
+    {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+    }
 }
